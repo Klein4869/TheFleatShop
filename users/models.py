88ecde3a3d4498c59e77b8e0d5524b2412181a59
klein4869 <thd1998@live.cn>
2018-04-25 02:51:03 +0800

    []

--- users/models.py
+++ users/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -6,3 +7,8 @@ from django.db import models
 #     username = models.CharField(max_length=30)
 #     password = models.CharField(max_length=30)
 #     email = models.EmailField()
+
+class product(models.Model):
+    product_name = models.CharField(max_length = 75)
+    product_price = models.IntegerField()
+    user = models.ManyToManyField(User)
